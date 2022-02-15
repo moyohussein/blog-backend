e4621f051517cd4f364429454064e5013c492804
Hussein AbdulQohar <aqmhussein@gmail.com>
2022-02-15 20:45:54 +0100

    first commit

---
+++ blog/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '90295fdfeca7a4ac5c641746f4dc0bee'),
+    },
+  },
+});
---
+++ blog/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '212212ca-2434-4a18-8d0f-86cb27f98a90'
+};
